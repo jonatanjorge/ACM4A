4485c8255f5135e490da8ed62254ddf7600cc6d2
Tomi <tomasmateo10@gmail.com>
2017-11-15 11:25:55 -0300

    Modificacion de boton y ruta de logout y la vista de inicio

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -220,7 +220,11 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs">
+                            {{
+                                \Illuminate\Support\Facades\Auth::user()->nombre
+                            }}
+                        </span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
@@ -252,14 +256,10 @@
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="{{ route("logout") }}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
-                </li>
-                <!-- Control Sidebar Toggle Button -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
                 </li>
             </ul>
         </div>
